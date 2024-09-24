0d98979b56cc3816727075051d34559f04d5a28f
Tram <tramnhoang@gmail.com>
2024-09-24 08:59:34 -0700

    Tram changes function_d

--- function_d.py
+++ function_d.py
@@ -2,7 +2,7 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
+    print("This message is just a message.")
 
 
 if __name__ == "__main__":
