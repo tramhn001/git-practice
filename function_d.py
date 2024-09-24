8e514b0346164db1eedbfe45d11796f336de45ea
Madeline Bennett <madeline.bennett.pm@gmail.com>
2024-09-25 02:00:33 +1000

    "wrote function d"

--- function_d.py
+++ function_d.py
@@ -2,8 +2,15 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
+    largest_number=0
+    largest_number_list=[]
+    for number in list: 
+        if number >= largest_number:
+            largest_number == number
+    return largest_number
 
+
+    pass
 
 if __name__ == "__main__":
     print(max_value([1, 12, 2, 42, 8, 3]))
